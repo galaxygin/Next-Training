8feedf2784405afca0ccead24d3d8b4c8d1f64e6
Galaxy <galaxygin7250@gmail.com>
2021-07-21 14:39:21 +1000

    Added functions to the DatabaseRequest

--- api/request/DatabaseRequest.tsx
+++ api/request/DatabaseRequest.tsx
@@ -22,6 +22,32 @@ export function getMemberProfile(docId: string): Promise<Member> {
     })
 }
 
+export function searchMember(name: string): Promise<Member> {
+    return new Promise(resolve => {
+        Firestore.collection("users").where('name', '==', name).get().then(snapshot => {
+            snapshot.forEach(doc => {
+                resolve({ "name": doc.data()?.name as string, "role": doc.data()?.role, "uri": "" })
+            })
+        })
+    })
+}
+
+
+//I defined the functions below but not connecting to the pages
+//Please replace the name of Collections with your own and connect to the pages
+
 export function addMember(member: Member) {
     return Firestore.collection("users").add(member)
+}
+
+export function addMemberWithId(docId: string, member: Member) {
+    return Firestore.collection("users").doc(docId).set(member)
+}
+
+export function updateMemberInfo(uid: string, name: string, profile: string) {
+    return Firestore.batch().update(Firestore.collection("users").doc(uid), { 'name': name, "profile": profile }).commit()
+}
+
+export function removeMember(uid: string) {
+    return Firestore.collection("users").doc(uid).delete()
 }
